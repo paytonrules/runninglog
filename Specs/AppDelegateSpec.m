89b062176b971230f9a9275169e6086d5ca11192
Eric Smith <paytonrules@gmail.com>
2012-05-08 18:45:45 -0500

    Clean up this spec

--- Specs/AppDelegateSpec.m
+++ Specs/AppDelegateSpec.m
@@ -5,14 +5,20 @@
  
 CONTEXT(AppDelegate)
 {
+    __block AppDelegate *delegate;
+    __block UITabBarController *tabController;
+    
     describe(@"toBeFailing",
+             
+             beforeEach(^{
+                delegate = [[AppDelegate alloc] init ];
+                delegate.window = [[UIWindow alloc] init];        
+                tabController = [[UITabBarController alloc] init];
+                delegate.window.rootViewController = tabController;
+             }),
+             
              it(@"adds a log run controller to the root view controller", 
                 ^{
-                    AppDelegate *delegate = [[AppDelegate alloc] init ];
-                    delegate.window = [[UIWindow alloc] init];
-                    UITabBarController *tabController = [[UITabBarController alloc] init];
-                    delegate.window.rootViewController = tabController;
-                    
                     [delegate application:nil didFinishLaunchingWithOptions:nil];
                     
                     LogRunController *cont = [tabController.viewControllers objectAtIndex:0];
@@ -22,11 +28,6 @@ CONTEXT(AppDelegate)
              
              it(@"adds a listRuns controller to the root view controller",
                 ^{
-                    AppDelegate *delegate = [[AppDelegate alloc] init ];
-                    delegate.window = [[UIWindow alloc] init];
-                    UITabBarController *tabController = [[UITabBarController alloc] init];
-                    delegate.window.rootViewController = tabController;
-                    
                     [delegate application:nil didFinishLaunchingWithOptions:nil];
                     
                     ListRunsController *cont = [tabController.viewControllers objectAtIndex:1];
